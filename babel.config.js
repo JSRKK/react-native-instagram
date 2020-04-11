bbb219c65d138153755b3c65cad56edac0c5da5a
Cypher8 <seera.kaenkaew8@gmail.com>
2020-04-11 21:42:14 +0700

    feat: babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [".ios.js", ".android.js", ".js", ".json"],
+        alias: {
+          'src': './src',
+          'assets': './src/assets',
+          'components': './src/components',
+          'screens': './src/screens'
+        },
+      },
+    ],
+  ],
 };
